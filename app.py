099efd8b5483ca54d8d357213d1173b1fe705718
Roshan <90826638+roshanx0@users.noreply.github.com>
2026-09-18 12:15:27 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,47 @@
+import gradio as gr
+import joblib
+import pandas as pd
+import os
+
+# Load model
+model = joblib.load("Student_Std_Hrs.pkl")
+
+
+def predict_result(study_hours):
+
+    input_data = pd.DataFrame({
+        "Study_Hours": [study_hours]
+    })
+
+    prediction = model.predict(input_data)[0]
+    probability = model.predict_proba(input_data)[0]
+
+    if prediction == 1:
+        result = "PASS"
+        confidence = probability[1] * 100
+    else:
+        result = "FAIL"
+        confidence = probability[0] * 100
+
+    return f"Student Result: {result}\nProbability: {confidence:.2f}%"
+
+
+demo = gr.Interface(
+    fn=predict_result,
+    inputs=gr.Number(
+        label="Enter Study Hours",
+        minimum=0,
+        maximum=24,
+        value=5
+    ),
+    outputs=gr.Textbox(label="Prediction"),
+    title="Student Result Prediction",
+    description="Predict Pass or Fail based on Study Hours."
+)
+
+
+if __name__ == "__main__":
+    demo.launch(
+        server_name="0.0.0.0",
+        server_port=int(os.environ.get("PORT", 7860))
+    )
